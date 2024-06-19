6c9f7f7fe0daf91a4582ee05c345e1863dc57bab
Durk006 <141444511+Durk006@users.noreply.github.com>
2024-06-18 18:01:07 -0600

    Week4 Update #1

--- app/page.js
+++ app/page.js
@@ -15,6 +15,9 @@ export default function Page() {
         <li>
           <Link className={linkStyles} href="week-3">Week 3</Link>
         </li>
+        <li>
+          <Link className={linkStyles} href="week-4">Week 4</Link>
+        </li>
       </ul>
 
     </main>
